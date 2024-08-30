b87b432a256092be25c47df9100c8833fb2290d9
lakhan soni <pru71843@adobe.com>
2024-08-30 18:44:00 +0530

    GBN

--- blocks/global-network/global-network.js
+++ blocks/global-network/global-network.js
@@ -1,42 +1,43 @@
-export default function decorate(block) {
-    let globalNetworkCont = [];
-    const div = document.createElement('div');
-    div.classList.add('region');
+// export default function decorate(block) {
+//     let globalNetworkCont = [];
+//     const div = document.createElement('div');
+//     div.classList.add('region');
 
-    // Process each row in the block
-    [...block.children].forEach((row, rowIndex) => {
-        if (rowIndex === 0) {
-            // On the first row, collect children of paragraph elements
-            [...row.children].forEach((p) => {
-                globalNetworkCont = [...p.children];
-            });
-        } else {
-            // For other rows, find anchor tags and modify them
-            const a = row.querySelector('a');
-            if (a) {
-                // Create and append a span to the anchor
-                const span = document.createElement('span');
-                span.classList.add('icon', 'ups-icon-right-arrow');
-                a.append(span);
+//     // Process each row in the block
+//     [...block.children].forEach((row, rowIndex) => {
+//         if (rowIndex === 0) {
+//             // On the first row, collect children of paragraph elements
+//             [...row.children].forEach((p) => {
+//                 globalNetworkCont = [...p.children];
+//             });
+//         } else {
+//             // For other rows, find anchor tags and modify them
+//             const a = row.querySelector('a');
+//             if (a) {
+//                 // Create and append a span to the anchor
+//                 const span = document.createElement('span');
+//                 const paragraph = document.createElement('p');
+//                 span.classList.add('icon', 'ups-icon-right-arrow');
+//                 a.append(span);
+//                 //paragraph.append(a);
+//                 // Append the modified anchor to the div
+//                 div.append(a);
                 
-                // Append the modified anchor to the div
-                div.append(a);
-                
-                // Remove the processed row
-                row.replaceWith('');
-            }
-        }
-    });
-    // Insert the div after the second item in globalNetworkCont
-    globalNetworkCont.forEach((row, index)=>{
-        if(index==1){
-            row.classList.add('gnb-desc');
-            row.insertAdjacentElement('afterend', div);
-        }
-        if(globalNetworkCont.length-1 === index){
-            row.classList.add('gnb-efmap');
-        }
-    })
+//                 // Remove the processed row
+//                 row.replaceWith('');
+//             }
+//         }
+//     });
+//     // Insert the div after the second item in globalNetworkCont
+//     globalNetworkCont.forEach((row, index)=>{
+//         if(index==1){
+//             row.classList.add('gnb-desc');
+//             row.insertAdjacentElement('afterend', div);
+//         }
+//         if(globalNetworkCont.length-1 === index){
+//             row.classList.add('gnb-efmap');
+//         }
+//     })
 
 
-}
+// }
