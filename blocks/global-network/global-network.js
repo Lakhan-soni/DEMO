f705d4f192e9b806ca1cfc8a15b31f919e3b59c1
lakhan soni <pru71843@adobe.com>
2024-09-04 15:26:32 +0530

    gnb

--- blocks/global-network/global-network.js
+++ blocks/global-network/global-network.js
@@ -1,22 +1,17 @@
 import { createKeyLine } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-    // Add key lines to all <h2> elements
-    block.querySelectorAll('h2').forEach(createKeyLine);
-
-    // Add a class to the first <p> element
-    const paragraphs = block.querySelectorAll('p');
-    if (paragraphs.length > 0) {
-        paragraphs[0].classList.add('global-network-description');
+  block.querySelectorAll('h2').forEach(createKeyLine);
+  const paragraphs = block.querySelectorAll('p');
+  if (paragraphs.length > 0) {
+    paragraphs[0].classList.add('global-network-description');
+  }
+  const buttons = block.querySelectorAll('.button-container');
+  buttons.forEach((button, index) => {
+    if (index < buttons.length - 1) {
+      button.classList.add('region-btn');
+    } else {
+      button.classList.add('exp-facility-map-btn');
     }
-
-    // Add classes to each .button-container element
-    const buttons = block.querySelectorAll('.button-container');
-    buttons.forEach((button, index) => {
-        if (index < buttons.length - 1) {
-            button.classList.add('region-btn');
-        } else {
-            button.classList.add('exp-facility-map-btn');
-        }
-    });
+  });
 }
