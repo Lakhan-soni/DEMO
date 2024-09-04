aef545108653ba75bb6d3eabfff77216a243a132
lakhan soni <pru71843@adobe.com>
2024-09-04 15:15:15 +0530

    gnb

--- blocks/global-network/global-network.js
+++ blocks/global-network/global-network.js
@@ -1,14 +1,22 @@
-
 import { createKeyLine } from '../../scripts/scripts.js';
-export default function decorate(block) {
-block.querySelectorAll('h2').forEach(createKeyLine);
-block.querySelectorAll('p')[0].classList.add('global-network-description');
-block.querySelectorAll('.button-container').forEach((button, index) => {
-    if(index < block.querySelectorAll('.button-container').length-1){
-        button.classList.add('region-btn');
-    }else{
-        button.classList.add('exp-facility-map-btn');
-    }
-});
 
+export default function decorate(block) {
+    // Add key lines to all <h2> elements
+    block.querySelectorAll('h2').forEach(createKeyLine);
+
+    // Add a class to the first <p> element
+    const paragraphs = block.querySelectorAll('p');
+    if (paragraphs.length > 0) {
+        paragraphs[0].classList.add('global-network-description');
+    }
+
+    // Add classes to each .button-container element
+    const buttons = block.querySelectorAll('.button-container');
+    buttons.forEach((button, index) => {
+        if (index < buttons.length - 1) {
+            button.classList.add('region-btn');
+        } else {
+            button.classList.add('exp-facility-map-btn');
+        }
+    });
 }
